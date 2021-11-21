ecaf65cc5e64015903d8ab37e97cccb66b8688db
Tim Allen <tallen@integsys.biz>
2021-11-21 13:22:05 -0500

    initial check-in

---
+++ verilog/RGMIIulator_top.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:20:32 11/21/2021 
+// Design Name: 
+// Module Name:    RGMIIulator_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module RGMIIulator_top(
+    input uart_rx,
+    output uart_tx,
+    input rgm0_en,
+    input rgm0_clk,
+    input [3:0] rgm0_d
+    );
+
+
+endmodule
