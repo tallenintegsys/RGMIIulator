e84e69758ae0b205a3c01e6259f7df37df6d845e
Tim Allen <tallen@integsys.biz>
2021-11-21 15:12:53 -0500

    fixin' to test

--- verilog/RGMIIulator_top.v
+++ verilog/RGMIIulator_top.v
@@ -1,4 +1,3 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -18,13 +17,43 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module RGMIIulator_top(
-    input uart_rx,
-    output uart_tx,
-    input rgm0_en,
-    input rgm0_clk,
-    input [3:0] rgm0_d
-    );
+`timescale 1ns / 1ps
+`include "uart_tx.v"
 
+module RGMIIulator_top(
+	input CLK,
+	input SW0,
+	input uart_rx,
+	output uart_tx,
+	output uart_busy,
+	input rgm0_en,
+	input rgm0_clk,
+	input [3:0] rgm0_d
+);
+
+wire clk = CLK;
+wire reset = SW0;
+reg uart_dv = 0;
+reg [7:0] uart_cout = 0;
+
+uart_tx #(.CLKS_PER_BIT((100000000)/115200)) uart_tx_0 (
+	.i_Clock(clk),
+	.i_TX_DV(uart_dv),			// cout is valid
+	.i_TX_Byte(uart_cout),		// char to send
+	.o_TX_Active(uart_busy),
+	.o_TX_Serial(uart_tx),
+	.o_TX_Done()
+);
+
+
+always @(posedge clk)
+	if (reset) begin
+		uart_dv <= 0;
+	end else begin
+		if (!uart_busy) begin
+			uart_cout <= "a";
+			uart_dv <= 1;
+		end
+	end
 
 endmodule
