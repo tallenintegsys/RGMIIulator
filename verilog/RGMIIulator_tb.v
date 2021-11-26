14a4383f0fd93eed4011717fce699c86b7c29a5b
Tim Allen <tim.allen@base2engineering.com>
2021-11-26 18:00:44 -0500

    Test bench input is complete

--- verilog/RGMIIulator_tb.v
+++ verilog/RGMIIulator_tb.v
@@ -39,9 +39,13 @@ assign	d0 = rgm0_d[0];
 
 localparam dst = 48'h54_ff_01_21_23_24;
 localparam src = 48'h12_34_56_78_9a_bc;
-localparam type =16'h0000;
+localparam type =16'h1234;
+localparam data = "Twas' on the good ship Venus...";
+localparam crc = 32'hfb029064; //we can ignore this
 
 integer i;
+reg [7:0]c;
+
 initial begin
 	$dumpfile("RGMIIulator.vcd");
 	$dumpvars(0, RGMIIulator_tb);
@@ -81,11 +85,26 @@ initial begin
 	#40 rgm0_d = {src[0],src[1],src[2],src[3]};
 	#40 rgm0_d = {src[4],src[5],src[6],src[7]};
 	//type
-	#40 rgm0_d = type[15:12];
-	#40 rgm0_d = type[11:8];
-	#40 rgm0_d = type[7:4];
-	#40 rgm0_d = type[3:0];
-
+	#40 rgm0_d = {type[8],type[9],type[10],type[11]};
+	#40 rgm0_d = {type[12],type[13],type[14],type[15]};
+	#40 rgm0_d = {type[0],type[1],type[2],type[3]};
+	#40 rgm0_d = {type[4],type[5],type[6],type[7]};
+	//payload
+	for (i=0; i<32; i=i+1) begin
+		c = data[i];
+		#40 rgm0_d = {c[0],c[1],c[2],c[3]};
+		#40 rgm0_d = {c[4],c[5],c[6],c[7]};
+	end
+	//crc
+	#40 rgm0_d = {crc[24],crc[25],crc[26],crc[27]};
+	#40 rgm0_d = {crc[28],crc[29],crc[30],crc[31]};
+	#40 rgm0_d = {crc[16],crc[17],crc[18],crc[19]};
+	#40 rgm0_d = {crc[20],crc[21],crc[22],crc[23]};
+	#40 rgm0_d = {crc[8],crc[9],crc[10],crc[11]};
+	#40 rgm0_d = {crc[12],crc[13],crc[14],crc[15]};
+	#40 rgm0_d = {crc[0],crc[1],crc[2],crc[3]};
+	#40 rgm0_d = {crc[4],crc[5],crc[6],crc[7]};
+	//we're done
 	#100	$finish;
 end
 
